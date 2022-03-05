a9a4625df274e8ca2da881f78cbdc0a8cef7d557
Nick Ilin <ninikitos90@gmail.com>
2022-03-05 11:19:36 -0500

    working on card class

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,77 @@
 import { showImagePopup } from './modal.js';
-import { cardTemplate,
-         cardsContainer } from '../utils/constants.js';
+import { cardTemplate, cardsContainer } from '../utils/constants.js';
+
+export default class Card {
+  constructor({ cardData, handleLikeClick, handleDeleteClick }, userId, cardSelector){
+    this._id = cardData._id;
+    this._name = cardData.name;
+    this._link = cardData.link;
+    this._likes = cardData.cardId;
+    this._ownerId = cardData.owner._id;
+
+    this._userId = userId;
+
+    this._handleLikeClick = handleLikeClick;
+
+    this._cardSelector = cardSelector;
+  }
+
+  getView() {
+    this._element = this._getTemplate();
+
+    this._element.querySelector('.card__title').textContent = this._name;
+    const image = this._element.querySelector('.card__image');
+    image.src = this._link;
+    image.alt = this._name; 
+
+    //Как ставить лайк? 
+    //const likeElement = this._element.querySelector('.card__like-button');
+    // if(isLiked){
+    //   this.element.classList.add('card__like-button_active');
+    // }
+
+    //const likeCounterElement = this._element.querySelector('.card__like-counter');
+    //likeCounterElement.textContent = this._likes.length.toString();
+
+    //Listeners
+  }
+
+  isLiked() {
+    return Boolean(this._likes.find((user) => {user._id === this._userId}));
+  }
+
+  updateLikes(cardData) {
+    this._likes = cardData.likes;
+    this._updateLikesView();
+  }
+
+  remove() {
+    this._element.remove();
+    this._element = null;
+  }
+
+  _getTemplate() {
+    return document
+      .querySelector(this._cardSelector)
+      .content
+      .querySelector('.card')
+      .cloneNode(true);
+  }
+
+  _setEventListeners() {
+    
+  }
+
+  _updateLikesView() {
+    const like = this.element.querySelector('.card__like-counter');
+    this._element.querySelector('.card__like-counter').textContent = this._likes.length.toString();
+    if(this.isLiked()){
+      like.classList.add('card__like-button_active');
+    } else {
+      like.classList.remove('card__like-button_active');
+    }
+  }
+}
 
 // функция задает обработчики всем кнопкам карточки,
 // и обработчик клика по картинке (просмотр фото)
